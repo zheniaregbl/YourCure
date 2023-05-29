38d54c5ea687c15920655c64f65750a92912521e
zhenia_regbl <zhenieregbl@gmail.com>
2023-05-29 23:45:42 +0300

    Add SplashActivity

---
+++ app/src/main/java/com/example/projectdraft1/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.projectdraft1.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.projectdraft1.R
+
+@Suppress("DEPRECATION")
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1200)
+    }
+}
